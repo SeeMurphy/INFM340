366db3658482e5bc5a35abaa4bd3b71e5dc49254
seemurf <79239700+seemurf@users.noreply.github.com>
2021-09-03 19:33:07 -0400

    Update netflixstock.py

--- netflixstock.py
+++ netflixstock.py
@@ -9,4 +9,20 @@ rows = []
 with open(filename, 'r') as csvfile:
     # csv reader object
     csvreader = csv.reader(csvfile)
+    # extracting field names
+    fields = next(csvreader)
+    # extracting data rows
+    for row in csvreader:
+        rows.append(row)
+    # total number of rows
+    print("Total no. of rows: %d"%(csvreader.line_num))
+# printing the field names
+print('Field names are:' + ', '.join(field for field in fields))
+# printing the first 5 rows
+print('\nFirst 5 rows are:\n')
+for row in rows[:5]:
+    #parsing
+    for col in row:
+        print("%10s"%col),
+    print('\n')
 
